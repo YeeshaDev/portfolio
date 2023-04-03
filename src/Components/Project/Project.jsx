83511f11e7b28a38369c380c75edbb4677235d0a
YeeashaDev <aishaagunbiade05@gmail.com>
2023-04-03 19:19:01 +0100

    portfolio

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { projects } from './data';
 import './Project.css';
 
 function Project() {
@@ -7,25 +8,27 @@ function Project() {
            
             <h1 className='work'>Projects</h1>
             <span className='my-projects'>my works</span>
-            <div className='project-wrapper project1' data-aos="fade-up">
-            <div className='project-details'>
-                <h1>Foodie Restuarant</h1>
-            <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores, expedita voluptates? Ut corrupti 
-                pariatur inventore facilis reprehenderit, 
-                aut tenetur iure.</h4>
-         <a href='https://foodie-restuarant-yeeshadev-fuzekzzfm-yeeshadev.vercel.app/'
+            
+                {projects.map((item) => {
+                    const{id,name,text,demoLink,codeLink,mobileImg,LaptopImg} = item;
+                    return (
+                        <div className='project-wrapper project1' key={id} data-aos="fade-left">
+                            <div className='project-details'>
+                <h1>{name}</h1>
+            <p>{text}</p>
+         <a href={demoLink}
          className='project-links'><span>
             View Demo</span></a> 
-            <a href='https://github.com/YeeshaDev/foodie-restuarant'
+            <a href={codeLink}
             className='project-links'><span>
             Source Code</span></a> 
             </div>
                 <div className='project-info '>
                     <div className='device'>
-                    <img src='./images/foodie-mac.png'
+                    <img src={LaptopImg}
                     alt='project'
                     className='device-img'/>
-                    <img src='./images/foodie-mobile.png'
+                    <img src={mobileImg}
                     alt='project'
                     className='mobile-img'/>
                     </div>
@@ -33,33 +36,17 @@ function Project() {
                
             </div>
             </div>
-            <div className='project-wrapper project2'data-aos="fade-left">
-            <div className='project-details'>
-                <h1>Gadgets</h1>
-            <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores, expedita voluptates? Ut corrupti 
-                pariatur inventore facilis reprehenderit, 
-                aut tenetur iure.</h4>
-         <a href='https://youtube-clone-yeeshadev.vercel.app'
-         className='project-links'><span>
-            View Demo</span></a> 
-            <a href='https://github.com/YeeshaDev/gadget-store-app'
-            className='project-links'><span>
-            Source Code</span></a> 
-            </div>
-                <div className='project-info '>
-                <div className='device'>
-                    <img src='./images/gadgets-tab.png'
-                    alt='project'
-                    className='device-img'/>
-                    <img src='./images/gadgets-mobile.png'
-                    alt='project'
-                    className='mobile-img'/>
-                    </div>
-                    
-               
-            </div>
-            </div>
-            <div className='project-wrapper project3' data-aos="fade-right">
+                    )
+                })}
+                
+            <hr/> 
+        </div>
+    )
+}
+
+export default Project
+
+/* <div className='project-wrapper ' data-aos="fade-right">
             <div className='project-details'>
                 <h1>Youtube Clone</h1>
             <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores, expedita voluptates? Ut corrupti 
@@ -78,63 +65,4 @@ function Project() {
                     className='project-img'/>
                
             </div>
-            </div>
-            <div className='project-wrapper project2'data-aos="fade-left">
-            <div className='project-details'>
-                <h1>TopSeal Blog</h1>
-            <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores, expedita voluptates? Ut corrupti 
-                pariatur inventore facilis reprehenderit, 
-                aut tenetur iure.</h4>
-         <a href='https://news-media-d3jm.vercel.app/'
-         className='project-links'><span>
-            View Demo</span></a> 
-            <a href='https://github.com/YeeshaDev/News-media'
-            className='project-links'><span>
-            Source Code</span></a> 
-            </div>
-                <div className='project-info '>
-                <div className='device'>
-                    <img src='./images/blog-laptop.png'
-                    alt='project'
-                    className='device-img'/>
-                    <img src='./images/blog-mobile.png'
-                    alt='project'
-                    className='mobile-img'/>
-                    </div>
-                    
-               
-            </div>
-            </div>
-            <div className='project-wrapper project2'data-aos="fade-right">
-            <div className='project-details'>
-                <h1>Musik</h1>
-            <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores, expedita voluptates? Ut corrupti 
-                pariatur inventore facilis reprehenderit, 
-                aut tenetur iure.</h4>
-         <a href='https://news-media-d3jm.vercel.app/'
-         className='project-links'><span>
-            View Demo</span></a> 
-            <a href='https://github.com/YeeshaDev/News-media'
-            className='project-links'><span>
-            Source Code</span></a> 
-            </div>
-                <div className='project-info '>
-                <div className='device'>
-                    <img src='./images/musik-tab.png'
-                    alt='project'
-                    className='device-img'/>
-                    <img src='./images/musik-mobile.png'
-                    alt='project'
-                    className='mobile-img'/>
-                    </div>
-                    
-               
-            </div>
-            </div>
-            
-            <hr/> 
-        </div>
-    )
-}
-
-export default Project
+            </div> */
---
+++ src/Components/Project/data.jsx
@@ -0,0 +1,64 @@
+export const projects =[
+    
+{
+id:1,
+name:'Foodie Restuarant',
+text:`This is a food website using mealdb api you can order variety of foods and reserve a
+table in advance.We have a blog post where you can search for your favorite foods get all the 
+ingredients and instructions.You can learn about us with one click.This website was created with NextJs,Mealdb Api,Tailwindcss,
+React-icons,React-redux Toolkit.`,
+LaptopImg:'./images/foodie-mac.png',
+mobileImg:'./images/foodie-mobile.png',
+demoLink:'https://foodie-restuarant-yeeshadev-fuzekzzfm-yeeshadev.vercel.app/',
+codeLink: 'https://github.com/YeeshaDev/foodie-restuarant'      
+},
+{
+id:2,
+name:'Gadgets',
+text:`This is a Gadget website where you can shop for different gadgets ranging from laptops,phones, speakers,games and so on
+.Using firebase you can upload your preferred profile picture,Sign in with google without going 
+ through the stress of filling forms.This website was created with ReactJs,CSS,Bootstrap,Firebase,React-Redux Toolkit,React Icons.`,
+LaptopImg:'./images/gadget-tab.png',
+mobileImg:'./images/gadgets-mobile.png' ,
+demoLink:'https://gadget-store-app.vercel.app/',
+codeLink:'https://github.com/YeeshaDev/gadget-store-app'  
+        
+},
+{
+id:3,
+name:'Topseal News Website',
+text:`This is a News Website where you can get latest news all around the world ranging from different 
+categories like technology,food,entertainment,business and so on.Check out the current news today 
+to stay updated. This website was created with NextJs,CSS,Bootstrap,New York Times Api.`,
+
+LaptopImg:'./images/blog-laptop.png',
+mobileImg:'./images/blog-mobile.png',
+demoLink:'https://news-media-d3jm.vercel.app/',
+codeLink:'https://github.com/YeeshaDev/News-media'
+        
+},
+{
+    id:4,
+    name:'Musik Streaming App',
+    text:`This is a Music streaming website where you can listen to snippets of different genres of 
+    music,search for your favorites artists and songs, popular songs in your country.
+    This website was created with ReactJs,Tailwindcss,React-Redux Toolkit,Shazam RapidApi.
+    `,
+     LaptopImg:'./images/musik-tab.png',
+mobileImg:'./images/musik-mobile.png',
+demoLink:'https://music-app-yeeshadev.vercel.app/',
+codeLink: 'https://github.com/YeeshaDev/music-app'     
+        
+},
+{
+id:5,
+name:'Netflix Movie Trailer App',
+text:`This is a netflix clone app that shows movie trailers of the specified movie.
+     This website was created with ReactJs,CSS,Firebase,Movie-trailer,Paystack payment gateway. `,
+LaptopImg:'./images/netflix-laptop.png',
+mobileImg:'./images/netflix-mobile.png',
+demoLink:'https://movie-app-clone-beta.vercel.app',
+codeLink: 'https://github.com/YeeshaDev/react-app-clones/tree/master/netflix_clone'  
+        
+},
+]
